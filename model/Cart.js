d669e2c1eaf90cc1ad7da18dd757b29651cd6c4f
minhnhut1250@gmail.com <minhnhut1250@gmail.com>
2018-11-21 21:50:05 +0700

    Update total bill

--- model/Cart.js
+++ model/Cart.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
+var MongoClient = require('mongodb').MongoClient; // connect online
+var uri = "mongodb://nhut:nhutlui123@ds042677.mlab.com:42677/test"; // connect online
+
 var Cart = new Schema({
   name 		:  String,
   date: String,
@@ -13,4 +16,6 @@ var Cart = new Schema({
   st 		: Number
 },{collection : 'cart'});
 
+
 module.exports = mongoose.model('Cart', Cart);
+
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var Cart = require('../model/Cart.js');
+var Cate = require('../model/Cate.js');
 
 var User = require('../model/User.js');
 
@@ -11,20 +12,78 @@ var LocalStrategy = require('passport-local').Strategy;
 
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('admin/login/login');
 });
 
+function TongDoanhThu(callback) {
+  MongoClient.connect(uri, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("3dwebsite");
+    dbo.collection("hoadon").find().toArray(function (err, result) {
+      if (err) {
+        throw err;
+        console.log(err);
+      } else if (result.length > 0) {
+        var tongket = {
+          giadung: 0,
+          fashion: 0,
+          mebe: 0,
+          thucung: 0,
+          xe: 0,
+          dientu: 0,
+        }
+        for (var i = 0; i < result.length; i++) {
+          var danhsach = result[i].danhsachsanpham;
+          tongket["giadung"] += parseInt(thongKe(danhsach, "giadung"));
+          tongket["fashion"] += parseInt(thongKe(danhsach, "fashion"));
+
+          tongket["thucung"] += parseInt(thongKe(danhsach, "thucung"));
+          tongket["xe"] += parseInt(thongKe(danhsach, "xe"));
+
+          tongket["dientu"] += parseInt(thongKe(danhsach, "dientu"));
+          tongket["mebe"] += parseInt(thongKe(danhsach, "mebe"));
+        }
+        var sum = 0;
+        for (var each in tongket) {
+          sum += tongket[each]
+        }
+        var result = {
+          tongSP: sum,
+          tongket: tongket
+        }
+        callback(result);
+      }
+    });
+    db.close();
+  });
+}
+
 /* GET home page. */
-router.get('/trang-chu', checkAdmin, function(req, res, next) {
-  res.render('admin/main/index');
+router.get('/trang-chu', function (req, res, next) {
+  Cart.find({
+    st: 2
+  }).then(function (data) {
+    var tongtien = 0;
+    for (var i = 0; i < data.length; i++) {
+      var cart = data[i].cart;
+      for (j = 0; j < cart.length; j++) {
+        var tien = cart[j].tien;
+        tongtien += tien;
+      }
+    }
+    res.render('admin/main/index',{
+      total:tongtien,
+    });
+  })
+  
 });
 
 router.post('/',
-  passport.authenticate('local', { 
+  passport.authenticate('local', {
     successRedirect: '/admin/trang-chu',
     failureRedirect: '/admin',
-    failureFlash: true 
+    failureFlash: true
   })
 );
 
@@ -33,60 +92,66 @@ passport.use(new LocalStrategy({
     passwordField: 'password'
   },
 
-  function(username, password, done) {
-      User.findOne({username: username}, function(err, username){
-          if(err) throw err;
-          if(username){
-            bcrypt.compare(password, username.password, function(err, user) {
-                if(err) throw err;
-                if(user){
-                     return done(null, username);
-                }else{
-                   return done(null, false, { message: 'Tài Khoảng Không Đúng' });
-                }
+  function (username, password, done) {
+    User.findOne({
+      username: username
+    }, function (err, username) {
+      if (err) throw err;
+      if (username) {
+        bcrypt.compare(password, username.password, function (err, user) {
+          if (err) throw err;
+          if (user) {
+            return done(null, username);
+          } else {
+            return done(null, false, {
+              message: 'Tài Khoảng Không Đúng'
             });
-          }else{
-             return done(null, false, { message: 'Tài Khoảng Không Đúng' });
           }
-      });
+        });
+      } else {
+        return done(null, false, {
+          message: 'Tài Khoảng Không Đúng'
+        });
+      }
+    });
   }
 
 ));
 
-passport.serializeUser(function(username, done) {
-  
+passport.serializeUser(function (username, done) {
+
   done(null, username.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, username) {
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, username) {
     done(err, username);
   });
 });
 
 
 
-router.post('/getUser',checkAdmin, function (req, res) {
-    res.json(req.user);
+router.post('/getUser', checkAdmin, function (req, res) {
+  res.json(req.user);
 });
 
-router.get('/dang-xuat',checkAdmin, function (req, res) {
+router.get('/dang-xuat', checkAdmin, function (req, res) {
   res.render('admin/login/login');
 });
 
-router.post('/dang-xuat',checkAdmin, function (req, res) {
-    req.logout();
-    res.redirect('/admin');
+router.post('/dang-xuat', checkAdmin, function (req, res) {
+  req.logout();
+  res.redirect('/admin');
 });
 
 
-function checkAdmin(req, res, next){
-   
-    if(req.isAuthenticated()){
-      next();
-    }else{
-      res.redirect('/admin');
-    }
+function checkAdmin(req, res, next) {
+
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/admin');
+  }
 }
 
 module.exports = router;
